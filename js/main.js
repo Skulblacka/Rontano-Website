66b28b291825b6a92432a2fcabb3cb49668ae86f
isha <ishashree@simpleshow.com>
2025-11-09 14:49:43 +0100

    add local website

---
+++ js/main.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", (event) => {
+    if (window.location.href.includes("links")) {
+        loadLinks();
+    }
+});
+
+function loadLinks() {
+    window.location.href = "links.html";
+}
+
+function loadLinks() {
+    window.location.href = "#band";
+}
